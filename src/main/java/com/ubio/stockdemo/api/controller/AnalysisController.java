1922b82c69ccc566980cc3b6a328278fae8ab3fd
pse7370 <pse7370@gmail.com>
2024-08-13 17:23:08 +0900

    종목별 프로그램매매 옵션 추가(시간별, 일자별), 일자별 조회 페이지 작업 중

--- src/main/java/com/ubio/stockdemo/api/controller/AnalysisController.java
+++ src/main/java/com/ubio/stockdemo/api/controller/AnalysisController.java
@@ -29,7 +29,18 @@ public class AnalysisController {
         return modelAndView;
     }
 
+    @GetMapping("/programDaily")
+    public ModelAndView getAnalysisProgramDaily(@RequestParam("code") long code, @RequestParam("date") String date, @RequestParam("name") String name) {
+        // 종목별프로그램매매추이(일별)
+        String data = analysisService.analysisDaily(code, date);
 
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("programDaily"); // 뷰 페이지
+        modelAndView.addObject("tradeList", data);
+        modelAndView.addObject("tName", name);
+        System.out.println(data);
+        return modelAndView;
+    }
 
 
 }
